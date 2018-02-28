396d4230c63a40fa06254b180c547ae3273424d7
Unknown <s179067@students.aie.edu.au>
2018-02-28 11:46:26 -0800

    PlayerReadt

---
+++ Assets/Caleb/GameStart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure;
+using UnityEngine.UI;
+
+public class GameStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
