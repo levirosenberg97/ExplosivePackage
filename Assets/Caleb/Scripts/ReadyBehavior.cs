7825af1faa2c1bfedc221c4642d55da0a17302c1
Unknown <s179067@students.aie.edu.au>
2018-03-05 15:13:47 -0800

    add players

--- Assets/Caleb/Scripts/ReadyBehavior.cs
+++ Assets/Caleb/Scripts/ReadyBehavior.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ReadyBehavior : MonoBehaviour {
-
+    public int players;
     public GameObject[] ReadyUp;
     PlayerIndex pIdx = PlayerIndex.One;
     GamePadState state;
@@ -27,6 +27,7 @@ public class ReadyBehavior : MonoBehaviour {
             if (ReadyUp[i].activeInHierarchy)
             {
                 check++;
+                players = check;
             }
         }
 
