337d43f8975f7803873632edb0f1cfe5bc277ad5
Unknown <s179067@students.aie.edu.au>
2018-03-07 11:54:49 -0800

    music

---
+++ Assets/Caleb/Scripts/GameAudio.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameAudio : MonoBehaviour {
+
+    public AudioSource[] sound;
+	// Use this for initialization
+	void Start ()
+    {
+        DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (!sound[0].isPlaying)
+        {
+            sound[1].Play();
+        }
+        else if (!sound[1].isPlaying)
+        {
+            sound[2].Play();
+        }
+        else if (!sound[2].isPlaying)
+        {
+            sound[0].Play();
+        }
+	}
+}
